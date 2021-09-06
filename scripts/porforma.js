acb3111dbd4be87eca93743e5537a85404fa415f
Atul Kumar <atul.kumar@glaucuslogistics.com>
2021-09-06 16:37:07 +0530

    main

--- scripts/porforma.js
+++ scripts/porforma.js
@@ -14,11 +14,43 @@ $(document).ready(function() {
         //     'Access-Control-Allow-Origin': "*"
         // },
         success: function(data){
-            alert(JSON.stringify(data));
+
+            var productDiv = "";
+            for(var i =0; i<data.field1.length;i++){
+                product = data.field1[i];
+                productDiv +=   '<div class="col-md-6 col-lg-4 mb-2">'
+                                +'<div class=" mb-12" >'
+                                + '<div class="card border-primary h-100">'
+                                    + '<div class="view zoom z-depth-2 rounded">'
+                                    +'<img class="img-fluid w-100 myImages"  style="width:100%; height: 100%;" id="img'+product.productId+'"  src="../images/about/about1.png"  >'
+                                    +'</div>'
+                                +'<div class="card-body"><div class="text-center pt-4">'
+                                +'<h6 class="text-dark font-weight-light lead fas">'+product.productName+'</h6></div></div>'
+                                +'<div class="card-footer bg-white">'
+                                +'<h6 class="mb-3"><span>&#8377</span><span class="font-weight-bold">'+product.productPrice+'</span></h6>'
+                                +'<div class="row">'+
+                                +'<div class="col-6"><button type="button" class="btn btn-primary btn-sm mr-1"><i class="fas fa-shopping-cart pr-2"></i>Add to cart</button>'
+                                +'</div>'
+                                +'<div class="col-6"><select class="lead"><option value="Select"></option></select>'
+                                +'</div></div>'
+                                +'</div></div>'
+                                +'</div></div>';
+            }
+
+            document.getElementById("products").innerHTML=productDiv;
+
+            
         },
         error: function(e){
-            alert(JSON.stringify(e));
+            alert("something unexpected happened");
         }
     });
 
  });
+
+
+
+
+
+
+ 
