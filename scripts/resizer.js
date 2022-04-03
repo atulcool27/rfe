680cb073a4b92b132f10a421f72f390d5fe0fb4c
Atul Kumar <kumaratulbusiness@gmail.com>
2022-04-03 16:13:24 +0530

    main

---
+++ scripts/resizer.js
@@ -0,0 +1,48 @@
+var domainName = 'https://racekonindustries.in';
+var imgWidth;
+var imgHeight;
+
+$(document).ready(function(){
+   $.ajax({
+       url: domainName+'/json/images.json',
+       type: 'GET',
+       success: function(data){
+        var obj;
+        for(var i=0;i<(data.page).length;i++){
+            if((data.page)[i].url === location.pathname){
+                obj = (data.page)[i];
+                break;
+            }
+        }
+
+        resizer(obj);
+    
+       },
+       error: function(e){
+           alert(e);
+       }
+   });
+});
+
+function resizer(obj){
+    var url = domainName+'/images/qualitypolicy/qualitypolicy.jpg';
+    getMeta(url,obj);
+   
+}
+
+
+function getMeta(url,obj) {   
+    var img = new Image();
+    img.onload = function() {
+        imgWidth = parseInt(this.width);
+        imgHeight = parseInt(this.height);
+        var imgRatio = imgWidth/imgHeight;
+        var calcval = (parseInt(window.screen.height) - parseInt(window.screen.width)/imgRatio);
+        console.log(calcval);
+        document.getElementById("heroCreator").innerHTML='<section id="hero" class="d-flex align-items-center" style="background-image: url(\'..'+obj.imageurl+'\');'+
+        ' height: calc(100vh - '+calcval+'px);"></section>';
+    };
+    img.src = url;
+}
+
+
